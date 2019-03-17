ed75825e039cf5437c12eff836fe30b315a449eb
PC <PC@PC-PC>
2019-03-17 16:53:58 +0700

    finish feature: CRUD

---
+++ AjaxJQueryCRUD/Controllers/EmployeeController.cs
@@ -0,0 +1,112 @@
+﻿using AjaxJQueryCRUD.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace AjaxJQueryCRUD.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ViewAll()
+        {
+            return View(GetAllEmployees());
+        }
+
+        public IEnumerable<Employee> GetAllEmployees()
+        {
+            using (AjaxJQueryDBEntities db = new AjaxJQueryDBEntities())
+            {
+                return db.Employees.ToList();
+            }
+
+        }
+
+        public ActionResult AddOrEdit(int id = 0)
+        {
+            Employee employee = new Employee();
+            if (id != 0)
+            {
+                using (AjaxJQueryDBEntities db = new AjaxJQueryDBEntities())
+                {
+                    employee = db.Employees.FirstOrDefault(x => x.EmployeeID == id);
+                }
+            }
+            return View(employee);
+        }
+
+        [HttpPost]
+        public JsonResult AddOrEdit(Employee employee)
+        {
+            try
+            {
+                if (employee.UpLoad != null)
+                {
+                    string fileName = Path.GetFileNameWithoutExtension(employee.UpLoad.FileName);
+                    string extension = Path.GetExtension(employee.UpLoad.FileName);
+                    fileName = fileName + DateTime.Now.ToString("yymmssfff") + extension;
+                    employee.ImagePath = @"~/Media/Images/" + fileName;
+                    employee.UpLoad.SaveAs(Path.Combine(Server.MapPath("~/Media/Images/"), fileName));
+                }
+                using (AjaxJQueryDBEntities db = new AjaxJQueryDBEntities())
+                {
+                    if (employee.EmployeeID == 0)
+                    {
+                        db.Employees.Add(employee);
+                        db.SaveChanges();
+                    }
+                    else
+                    {
+                        db.Entry(employee).State = EntityState.Modified;
+                        db.SaveChanges();
+                    }
+
+                }
+                return Json(new
+                {
+                    success = true,
+                    html = GlobalClass.RenderRazorViewToString(this, "ViewAll", GetAllEmployees()),
+                    message = "Submitted Successfully"
+                }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception e)
+            {
+                return Json(new { success = false, message = e.Message }, JsonRequestBehavior.AllowGet);
+
+            }
+        }
+
+        [HttpPost]
+        public JsonResult Delete(int id)
+        {
+            try
+            {
+                using (AjaxJQueryDBEntities db = new AjaxJQueryDBEntities())
+                {
+                    db.Employees.Remove(db.Employees.Find(id));
+                    db.SaveChanges();
+                    return Json(new
+                    {
+                        success = true,
+                        html = GlobalClass.RenderRazorViewToString(this, "ViewAll", GetAllEmployees()),
+                        message = "Deleted Successfully"
+                    }, JsonRequestBehavior.AllowGet);
+                }
+            }
+            catch (Exception e)
+            {
+                return Json(new { success = false, message = e.Message }, JsonRequestBehavior.AllowGet);
+
+            }
+        }
+    }
+}
---
+++ AjaxJQueryCRUD/Scripts/site.js
@@ -0,0 +1,107 @@
+﻿/// <reference path="jquery.validate.js" />
+/// <reference path="bootstrap.js" />
+/// <reference path="jquery.validate.unobtrusive.js" />
+/// <reference path="notify.min.js" />
+/// <reference path="jquery-3.3.1.min.js" />
+
+$(function () {
+    ActiveJqueryDataTable();
+    WaitAjax();
+});
+function ShowImagePreview(imageUpLoader, imagePreview) {
+    if (imageUpLoader.files && imageUpLoader.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            $(imagePreview).attr("src", e.target.result);
+        };
+        reader.readAsDataURL(imageUpLoader.files[0]);
+    }
+}
+function AjaxJQueryPost(form) {
+    $.validator.unobtrusive.parse(form);
+
+    if ($(form).valid()) {
+        var ajaxConfig = {
+            type: "POST",
+            url: form.action,
+            data: new FormData(form),
+            success: function (response) {
+                if (response.success){
+                    $("#viewAll").html(response.html);
+                    refreshAddNewTab($(form).attr("data-resetUrl"), true);
+                    $.notify(response.message, "success");
+                    if (typeof (ActiveJqueryDataTable) !== "undefined" && $.isFunction(ActiveJqueryDataTable)){
+                        ActiveJqueryDataTable();
+                    }
+                } else {
+                    $.notify(response.message, "error");
+                }
+               
+            }
+        };
+        if ($(form).attr("enctype") === "multipart/form-data") {
+            ajaxConfig["contentType"] = false;
+            ajaxConfig["processData"] = false;
+
+        }
+        $.ajax(ajaxConfig);
+    }
+    return false;
+
+}
+function refreshAddNewTab(resetUrl, showViewTab) {
+    $.ajax({
+        type: "GET",
+        url: resetUrl,
+        success: function (response) {
+            $("#addNew").html(response);
+            $("ul.nav.nav-tabs a:eq(1)").html("AddNew");
+            if (showViewTab) {
+                $("ul.nav.nav-tabs a:eq(0)").tab("show");
+
+            }
+      }
+    });
+}
+function Edit(url) {
+    $.ajax({
+        type: "GET",
+        url: url,
+        success: function (response) {
+            $("#addNew").html(response);
+            $("ul.nav.nav-tabs a:eq(1)").html("Edit");
+            $("ul.nav.nav-tabs a:eq(1)").tab("show");
+
+        }
+    });
+}
+
+function Delete(url) {
+    if (confirm('Are you sure to to delete this record?')) {
+        $.ajax({
+            type: "Post",
+            url: url,
+            success: function (response) {
+                if (response.success) {
+                    $("#viewAll").html(response.html);
+                    $("ul.nav.nav-tabs a:eq(1)").html("AddNew");
+                    $.notify(response.message, "success");
+                } else {
+                    $.notify(response.message, "error");
+                }
+            }
+        });
+    }
+
+}
+function ActiveJqueryDataTable() {
+    $("#employee-table").DataTable();
+}
+function WaitAjax() {
+    $("#loaderbody").addClass('hide');
+    $(document).bind('ajaxStart', function () {
+        $("#loaderbody").removeClass('hide');
+    }).bind('ajaxStop', function () {
+        $("#loaderbody").addClass('hide');
+    });
+}
